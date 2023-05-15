e92668b7bd4fdab26c7d5d0c979945af98dbceb8
Werner Anobile Schwarz <anobile@outlook.com>
2023-05-15 19:48:21 +0100

    Revert "app.py Update"
    This reverts commit 73801ae7d9fa990f23abf366cb4bffe156714add.

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 app = Flask(__name__)
-@app.route("/")
-def hello():
- return("Hello World!") 
+@app.route("/")#URL leading to method
+def hello(): # Name of the method
+ return("/html/index.html") #indent this line
 if __name__ == "__main__":
- app.run(host='0.0.0.0', port='8080')
+ app.run(host='0.0.0.0', port='8080') # indent this line
